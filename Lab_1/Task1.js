e0906ed63a6b3466e6e3c9ce1b36c3b834181167
Minister <straikermc999@gmail.com>
2019-09-19 18:33:05 +0300

    Callback Task 1

---
+++ Lab_1/Task1.js
@@ -0,0 +1,20 @@
+function loadscript(src,callback) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.onload = () => callback(null, script);
+    script.onerror = () => callback(new Error(`Не удалось загрузить скрипт ${src}`));
+  
+    document.head.append(script);  
+}
+function load(){
+    loadscript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js',
+    function(error, script)
+     {
+        if (error) {
+        alert("Error")
+        } else {
+        alert("Good")
+        }
+      }
+    );   
+}
--- Lab_1/Untitled-1.js
+++
@@ -1,2 +0,0 @@
-let TestJS = "Test";
-alarm(TestJS);
