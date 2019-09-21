e5291937528f20db40174ca0095bb8e17694913a
Minister <straikermc999@gmail.com>
2019-09-22 02:10:49 +0300

    Not Finnal

--- Lab_1/Task1.js
+++ Lab_1/Task1.js
@@ -1,19 +1,7 @@
-function loadscript(src,callback) {
-    let script = document.createElement('script');
-    script.src = src;
-    script.onload = () => callback(null, script);
-    script.onerror = () => callback(new Error(`Не удалось загрузить скрипт ${src}`));
-    document.head.append(script);  
-}
 function loadTask1(){
-    loadscript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js',
-    function(error, script)
-     {
-        if (error) {
-        alert("Error")
-        } else {
-        alert("Good")
-        }
-      }
-    );   
+  async function doLabs(subject, callback) {
+    alert(`Starting doing ${subject} labs`);
+    callback();
+  } 
+  doLabs('Testing',function(){alert('Stop do Labs')})
 }
--- Lab_1/Task2.js
+++ Lab_1/Task2.js
@@ -1,13 +1,12 @@
 function loadTask2(){
-let promise = new Promise(function(resolve,reject){
-    let src = 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js';
-    let script = document.createElement('script');
-    script.src = src;
-    script.onload = () => resolve("Good");
-    script.onerror = () => reject(new Error("Error Message"));
-    document.head.append(script);  
-}
-).then(result => alert(result),
-       error => alert(error) 
-);
+    const promise = new Promise((resolve, reject) => {
+        const randomNumber = Math.random();
+        setTimeout(() => {
+          if(randomNumber < .6) {
+            resolve('Good');
+          } else {
+            reject('Bad');
+        }
+        }, 2000);
+      }).then(alert).catch(alert);
 }
